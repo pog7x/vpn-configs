913d68df8431c6a887d6232cc3b84566a5bc2dde
pog7x <poluningm@gmail.com>
2026-01-22 10:14:11 +0300

    updates

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ HEADERS = {
     "User-Agent": (
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
         "AppleWebKit/537.36 (KHTML, like Gecko) "
-        "Chrome/138.0.0.0 Safari/537.36"
+        "Chrome/143.0.0.0 Safari/537.36"
     )
 }
 
